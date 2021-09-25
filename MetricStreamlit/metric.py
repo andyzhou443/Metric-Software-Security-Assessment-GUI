dca3f858364b8113f5d510ebcdd98f64d2fff0cd
nimajneben <47288387+nimajneben@users.noreply.github.com>
2021-09-25 13:25:03 -0400

    Update metric.py

--- MetricStreamlit/metric.py
+++ MetricStreamlit/metric.py
@@ -57,4 +57,5 @@ if submit:
         st.write("CountLineComment : ",totalCommentLineCount)
         st.write("CountLineCode : ",totalCodeLine)
         st.write("CountClass : ",totalClasses)
+        st.write("CountMethods:", totalMethods)
         st.write("RatioCommentToCode  : ",totalCommentLineCount/totalCodeLine)
