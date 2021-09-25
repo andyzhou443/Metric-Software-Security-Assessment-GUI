7893bd08bd94217aec395e85c301d7b6e3cfdc65
JJCai <jcai8242@bths.edu>
2021-09-25 14:35:23 -0400

    comparison update

--- MetricStreamlit/comparison.py
+++ MetricStreamlit/comparison.py
@@ -11,18 +11,26 @@ RATIOCOMMENTTOCODE_MEAN = 0.56208 # "RatioCommentToCode"
 # Param: Number userMetric, String METRIC_CODE
 # (UserMetric/DatabaseMetricMean) x 100
 # USE ONE OF CODES ABOVE TO TELL WHICH METRIC TO USE
-def percentMatch(userMetric, METRIC_CODE) {
+def percentMatch(userMetric, METRIC_CODE):
     if METRIC_CODE == "CountLine":
-    
+        return userMetric/COUNTER_MEAN
     elif METRIC_CODE == "CountLineBlank":
-    
+        return userMetric/TOTALBLANKLINECOUNT_MEAN
     elif METRIC_CODE == "CountLineComment":
-    
+        return userMetric/TOTALCOMMENTLINECOUNT_MEAN
     elif METRIC_CODE == "CountLineCode":
-    
+        return userMetric/TOTALCODELINE_MEAN
     elif METRIC_CODE == "CountClass":
-    
+        return userMetric/TOTALCLASSES_MEAN
     elif METRIC_CODE == "CountMethods":
-    
+        return userMetric/TOTALMETHODS_MEAN
     elif METRIC_CODE == "RatioCommentToCode":
-}
+        return userMetric/RATIOCOMMENTTOCODE_MEAN
+
+print(percentMatch(2, "CountLine"))
+print(percentMatch(2, "CountLineBlank"))
+print(percentMatch(2, "CountLineComment"))
+print(percentMatch(2, "CountLineCode"))
+print(percentMatch(2, "CountClass"))
+print(percentMatch(2, "CountMethods"))
+print(percentMatch(2, "RatioCommentToCode"))
