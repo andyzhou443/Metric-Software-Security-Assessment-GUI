41fa2873023fb9e7f5aa0f97b742a50efc711841
JJCai <jcai8242@bths.edu>
2021-09-25 14:13:33 -0400

    update comparison

--- MetricStreamlit/comparison.py
+++ MetricStreamlit/comparison.py
@@ -1,12 +1,12 @@
 
 # Hard coded constants that represent the mean of each metric in the spreadsheet
-COUNTER_MEAN = 0               # "CTR"
-TOTALBLANKLINECOUNT_MEAN = 0   # "
-TOTALCOMMENTLINECOUNT_MEAN = 0
-TOTALCODELINE_MEAN = 0
-TOTALCLASSES_MEAN = 0
-TOTALMETHODS_MEAN = 0
-RATIOCOMMENTTOCODE_MEAN = 0
+COUNTER_MEAN = 1008.96         # "CountLine"
+TOTALBLANKLINECOUNT_MEAN = 172.24   # "CountLineBlank"
+TOTALCOMMENTLINECOUNT_MEAN = 248 # "CountLineComment"
+TOTALCODELINE_MEAN = 592.232         # "CountLineCode"
+TOTALCLASSES_MEAN = 2.288      # "CountClass"
+TOTALMETHODS_MEAN = 0          # "CountMethods"
+RATIOCOMMENTTOCODE_MEAN = 0    # "RatioCommentToCode"
 
 # Param: Number userMetric, String METRIC_CODE
 # (UserMetric/DatabaseMetricMean) x 100
